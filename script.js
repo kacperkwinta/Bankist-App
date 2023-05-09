11bbbcc3f5dd49b830ce74edb9c30540c2306176
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-05-09 17:22:00 +0200

    ✨ new format of dates in movements

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-05-05T17:01:17.194Z',
+    '2023-05-06T23:36:17.929Z',
+    '2023-05-07T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -77,19 +77,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // DISPLAY MOVEMENTS
 ///////////////////////////////////////
 const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
+
   // sorting movements
   const movs = sort
     ? acc.movements.slice().sort((a, b) => a - b)
     : acc.movements;
-  containerMovements.innerHTML = '';
+
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? `deposit` : `withdrawal`;
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = ` 
         <div class="movements__row">
@@ -101,6 +100,26 @@ const displayMovements = function (acc, sort = false) {
         </div>`;
     containerMovements.insertAdjacentHTML(`afterbegin`, html);
   });
+};
+
+///////////////////////////////////////
+// CALC DATE
+///////////////////////////////////////
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 };
 
 ///////////////////////////////////////
